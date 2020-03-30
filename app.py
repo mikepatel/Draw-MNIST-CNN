8408eabc624ac8f6ff5600e6f66fb511840b37ea
Michael Patel <michael.patel94@gmail.com>
2020-03-30 17:21:47 -0400

    Can draw and classifygit rm -r results/

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ File description:
 ################################################################################
 # Imports
 import os
+import numpy as np
 import pygame
 from PIL import Image
 
@@ -23,10 +24,12 @@ SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 500
 WHITE = [255, 255, 255]
 BLACK = [0, 0, 0]
-RADIUS = 7
+RADIUS = 25
 DRAW = False
 LAST_POS = (0, 0)
 DONE = False
+LEFT_CLICK = 1
+RIGHT_CLICK = 3
 
 
 ################################################################################
@@ -60,8 +63,12 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 DONE = True
 
+            # clear screen
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == RIGHT_CLICK:
+                screen.fill(BLACK)
+
             # start drawing using mouse
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == LEFT_CLICK:
                 pygame.draw.circle(screen, WHITE, event.pos, RADIUS)
                 DRAW = True
 
@@ -73,19 +80,28 @@ if __name__ == "__main__":
                 LAST_POS = event.pos
 
             # stop drawing
-            if event.type == pygame.MOUSEBUTTONUP:
+            if event.type == pygame.MOUSEBUTTONUP and event.button == LEFT_CLICK:
                 DRAW = False
 
                 # save current screen image to feed into classifier
                 saved_image_filepath = os.path.join(os.getcwd(), "saved_images\\saved.png")
                 pygame.image.save(screen, saved_image_filepath)
-                image = Image.open(saved_image_filepath)
+                image = Image.open(saved_image_filepath).convert("L")  # convert to greyscale
 
                 # resize image
                 image = image.resize((28, 28))
-                image.save(saved_image_filepath)
+
+                # normalize image
+                image = np.array(image).astype(np.float32)
+
+                # reshape: (1, 28, 28, 1)
+                image = image.reshape(-1, 28, 28, 1)
 
                 # classify saved image
+                prediction = model.predict(image)
+                print(np.argmax(prediction))
+
+                # display prediction in secondary screen
 
             pygame.display.flip()
 
